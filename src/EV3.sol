3ea53692d5cfc36c65358420af8ce3b6bd26e733
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-21 14:37:40 -0400

    Readme change

--- src/EV3.sol
+++ src/EV3.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 /**
- * Governance token
+ * Governance token for the Achiev3 protocol.
  */
 contract EV3 is ERC20 {
     constructor() ERC20('EV3', 'EV3') {
