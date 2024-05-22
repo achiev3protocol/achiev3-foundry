c4baf8f683291f93a9882db6b8243121f73bac3a
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-22 06:00:13 -0400

    Comments

--- src/ContractRegistry.sol
+++ src/ContractRegistry.sol
@@ -9,17 +9,30 @@ import './interfaces/IContractRegistry.sol';
  * @dev Contract for managing the canonical addresses of other contracts in the protocol.
  */
 contract ContractRegistry is OwnableUpgradeable, IContractRegistry {
+    /**
+     * @dev Address of the current version of the AchievementSetRegistry contract.
+     */
     address public setRegistryAddress;
+
+    /**
+     * @dev Address of the current version of the UserRegistry contract.
+     */
     address public userRegistryAddress;
 
     function initialize() initializer public {
         __Ownable_init(msg.sender);
     }
 
+    /**
+     * @dev Sets the address of the current version of the AchievementSetRegistry contract.
+     */
     function setSetRegistryAddress(address _setRegistryAddress) external onlyOwner {
         setRegistryAddress = _setRegistryAddress;
     }
 
+    /**
+     * @dev Sets the address of the current version of the UserRegistry contract.
+     */
     function setUserRegistryAddress(address _userRegistryAddress) external onlyOwner {
         userRegistryAddress = _userRegistryAddress;
     }
