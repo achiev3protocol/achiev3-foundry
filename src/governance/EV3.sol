2106c93188eb7ab4280830f660ecbfa4776127d2
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-30 12:40:50 -0400

    Add ERC20Votes

--- src/governance/EV3.sol
+++ src/governance/EV3.sol
@@ -13,13 +13,22 @@
 pragma solidity ^0.8.13;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
 /**
  * @title EV3
  * @dev Governance token for the Achiev3 protocol.
  */
-contract EV3 is ERC20 {
-    constructor() ERC20('EV3', 'EV3') {
+contract EV3 is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20('EV3', 'EV3') ERC20Permit("EV3") {}
 
+    function _update(address from, address to, uint256 amount) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, amount);
+    }
+
+    function nonces(address owner) public view virtual override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
     }
 }
