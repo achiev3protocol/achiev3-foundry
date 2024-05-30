d094248aa2bd55c0b735ea1a6eba05658a38dc02
Thorne <ExistentialEnso@users.noreply.github.com>
2024-05-30 12:11:10 -0400

    Sick ASCII art

--- src/interfaces/IAchievementSetRegistry.sol
+++ src/interfaces/IAchievementSetRegistry.sol
@@ -1,3 +1,14 @@
+/*
+             _     _           ____  
+            | |   (_)         |___ \ 
+   __ _  ___| |__  _  _____   ____) |
+  / _` |/ __| '_ \| |/ _ \ \ / /__ < 
+ | (_| | (__| | | | |  __/\ V /___) |
+  \__,_|\___|_| |_|_|\___| \_/|____/ 
+
+  decentralized, permissionless achievements protocol
+ */
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
