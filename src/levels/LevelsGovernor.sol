804c0e35298c314038e956f79f6686ee807ac3dc
Thorne <ExistentialEnso@users.noreply.github.com>
2024-10-05 12:10:37 -0400

    Initial work on LevelsGovernor

---
+++ src/levels/LevelsGovernor.sol
@@ -0,0 +1,74 @@
+/*
+             _     _           ____  
+            | |   (_)         |___ \ 
+   __ _  ___| |__  _  _____   ____) |
+  / _` |/ __| '_ \| |/ _ \ \ / /__ < 
+ | (_| | (__| | | | |  __/\ V /___) |
+  \__,_|\___|_| |_|_|\___| \_/|____/ 
+
+  decentralized, permissionless achievements protocol
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import 'erc721a-upgradeable/contracts/ERC721AUpgradeable.sol';
+import '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+
+contract LevelsGovernor is ERC721AUpgradeable, OwnableUpgradeable {
+    /**
+     * @dev Mapping of addresses that are allowed to award xp.
+     */
+    mapping(address => bool) public canAwardXp;
+
+    /**
+     * @dev Mapping of addresses to their experience points.
+     */
+    mapping(address => uint256) public walletXp;
+
+    /**
+     * @dev Mapping of addresses to their token IDs.
+     */
+    mapping(address => uint256) public tokenIds;
+
+    /**
+     * @dev IPFS CID for the icon of the leaderboard
+     */
+    string public leaderboardIconCid;
+
+    address public contractRegistry;
+
+    constructor(string memory collectionName, string memory collectionSymbol, address _contractRegistry, address owner) initializerERC721A initializer {
+        contractRegistry = _contractRegistry;
+
+        __ERC721A_init(collectionName, collectionSymbol);
+        __Ownable_init(owner);
+    }
+
+    /**
+     * @dev Sets whether a user is allowed to award xp.
+     * @param user The address of the user to set the permission for.
+     * @param canAward Whether the user is allowed to award xp.
+     */
+    function setCanAwardXp(address user, bool canAward) public onlyOwner {
+        canAwardXp[user] = canAward;
+    }
+
+    /**
+     * @dev Awards xp to a user. If the user does not have a token for this leaderboard, they are minted one.
+     * @param user The address of the user to award xp to.
+     * @param amount The amount of xp to award.
+     */
+    function awardXp(address user, uint256 amount) public {
+        if (!canAwardXp[msg.sender]) revert NotAuthorized();
+
+        if(tokenIds[user] == 0) {
+            _mint(user, 1);
+            tokenIds[user] = _totalMinted();
+            walletXp[user] = amount;
+        } else {
+            walletXp[user] += amount;
+        }
+    }
+}
+
