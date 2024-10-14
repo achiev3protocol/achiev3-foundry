5700548c9e87c4a2357e3d6b61d5cf7d75c46cc1
Thorne <ExistentialEnso@users.noreply.github.com>
2024-10-13 21:23:41 -0400

    New struct

---
+++ src/structs/LevelSystem.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+struct LevelSystem {
+    string name;
+    string description;
+    uint256 xpMultiplier;
+    uint256 initialLevelUpXp;
+    uint256 maxLevel;
+}
+
